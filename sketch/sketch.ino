a3b25a5071fd4843e95232100999bfb372717bdf
Daniel Binoy <dbinoy15@gmail.com>
2018-06-23 23:37:09 -0700

    cleaned code

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -13,8 +13,8 @@ char keys[ROWS][COLS] = {
 byte rowPins[ROWS] = {5,4,3,2};
 byte colPins[COLS] = {9,8,7,6};
 
-char correctPin[4] = {'1','2','3','4'};
-char enteredPin[4] = {'0','0','0','0'};
+char correctPin[] = "1234";
+char enteredPin[] = "0000";
 int currentSpot = 0;
 
 Keypad keypad = Keypad(makeKeymap(keys),rowPins, colPins, ROWS, COLS);
@@ -30,13 +30,7 @@ void loop() {
     currentSpot = currentSpot+1;
     if(currentSpot == 4){
       Serial.println(enteredPin);
-      bool yes = true;
-      for(int i = 0;i<4;i++){
-        if( strcmp(enteredPin[i], correctPin[i]) ){
-          yes = false;
-        }
-      }
-      if(yes){
+      if(strcmp(enteredPin,correctPin) == 0){
         Serial.println("correct!");
       }else{
         Serial.println("wrong!");
