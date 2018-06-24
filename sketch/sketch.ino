11804f0797fa90881665ab1764fd0f2f8b19caa1
Daniel Binoy <dbinoy15@gmail.com>
2018-06-23 22:23:50 -0700

    flipped char matrix
    revered rows and columns of the actual array. it seems to work lol

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -3,17 +3,17 @@
 const byte ROWS = 4;
 const byte COLS = 4;
 
-char keys[COLS][ROWS] = {
-  {'1','2','3','A'},
-  {'4','5','6','B'},
-  {'7','8','9','C'},
-  {'*','0','#','D'},
+char keys[ROWS][COLS] = {
+  {'D','#','0','*'},
+  {'C','9','8','7'},
+  {'B','6','5','4'},
+  {'A','3','2','1'},
 };
 
 byte rowPins[ROWS] = {5,4,3,2};
 byte colPins[COLS] = {9,8,7,6};
 
-Keypad keypad = Keypad(makeKeymap(keys),colPins, rowPins, COLS, ROWS);
+Keypad keypad = Keypad(makeKeymap(keys),rowPins, colPins, ROWS, COLS);
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
